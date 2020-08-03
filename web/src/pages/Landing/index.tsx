8f4007f4fc82aad3a465921e9ac5f73215a05827
kalavh <jefferson_1996Th@hotmail.com>
2020-08-03 20:45:11 -0300

    Import Logo as make divs

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import logoSvg from '../../assets/images/logo.svg'
+
 function Landing () {
-    return <h1> Landing</h1>
+    return (
+        <div id="page-landing">
+            <div id="page-landing-content" className="container">
+                <div className="logo-container">
+                    <img src={logoSvg} alt="Proffy Logo" />
+
+                </div>
+            </div>
+        </div>
+
+
+    )
 }
 
 export default Landing;
