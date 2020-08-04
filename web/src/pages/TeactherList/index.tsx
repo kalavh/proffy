f0785d76d1c01ca893501a52e0bd6c361c6ea985
kalavh <jefferson_1996Th@hotmail.com>
2020-08-04 19:58:12 -0300

    start of header of teatcher

--- web/src/pages/TeactherList/index.tsx
+++ web/src/pages/TeactherList/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import backIcon from '../../assets/images/icons/back.svg'
+import logoImg from '../../assets/images/logo.svg'
 function TeacherList() {
     return (
-        <h1>Teacther List </h1>
+        <div id="page-teacher-list" className="container">
+            <header className="page-header">
+                <div className="top-bar-container"> 
+                    <Link to="/">
+                        <img src={backIcon} alt="voltar"></img>
+                    </Link>
+                    <img src={logoImg} alt="proffy"/>
+                </div>
+
+                <div className="header-content">
+                    <strong>They are our Teacther </strong>
+                </div>
+            </header>
+           
+        </div>
     )
 }
 
