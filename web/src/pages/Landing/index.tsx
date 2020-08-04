5c12c196dd9c6bd3ae3381b2d2651569774197be
kalavh <jefferson_1996Th@hotmail.com>
2020-08-03 21:00:58 -0300

    finish Html from this page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 import logoSvg from '../../assets/images/logo.svg'
+import heroSvg from '../../assets/images/landing.svg'
+import studyIcon from '../../assets/images/icons/study.svg'
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 
 function Landing () {
     return (
@@ -8,8 +12,29 @@ function Landing () {
             <div id="page-landing-content" className="container">
                 <div className="logo-container">
                     <img src={logoSvg} alt="Proffy Logo" />
-
+                    <h2> Your Study Website</h2>
                 </div>
+                <img
+                 src={heroSvg} 
+                 alt="Image plataform" 
+                 className="hero-image"
+                />
+                <div className="buttons-container">
+                    <a href="" className="buttons-container">
+                       <img src={studyIcon} alt="study Icon" />
+                       Study
+                    </a>
+
+                    <a href="" className="give-classes">
+                       <img src={giveClassesIcon} alt="study Icon" />
+                       Study
+                    </a>
+                    <a href="" className="study"> </a>
+                </div>
+                <span className="total-connections">
+                    more than 200 connections 
+                    <img src={purpleHeartIcon} alt="purple heart"/>
+                </span>
             </div>
         </div>
 
