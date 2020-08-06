6c19648fd2d88ce73cfdc59b0d5c6f701ceadb2f
kalavh <jefferson_1996Th@hotmail.com>
2020-08-06 20:48:50 -0300

    form from teatcher

--- web/src/pages/TeactherForm/index.tsx
+++ web/src/pages/TeactherForm/index.tsx
@@ -6,6 +6,25 @@ function TeatcherForm() {
         <div id="page-teacher-form" className="container">
         <PageHeader title="Thank you for teach people"
         description="Register on Proffy"/>
+
+        <main>
+            <fieldset>
+                <legend> Your data</legend>
+                    <div className="input-block">
+                        <label htmlFor="name">Full Name</label>
+                        <input type="text"  placeholder="name" id="name"/>
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="avatar">Avatar Link</label>
+                        <input type="text"  placeholder="avatar" id="avatar"/>
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="whatsapp">whatsapp</label>
+                        <input type="text"  placeholder="whatsapp" id="whatsapp"/>
+                    </div>
+                    
+            </fieldset>
+        </main>
     </div>
     )
 }
