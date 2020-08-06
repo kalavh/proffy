cbedd0e117ea29742e3221c4c965d969aa478a4c
kalavh <45084197+kalavh@users.noreply.github.com>
2020-08-06 14:23:18 -0300

    fix name of table

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -11,7 +11,7 @@ const connectionsController = new ConnectionsController();
 
 routes.get("/classes", classesControllers.index);
 routes.post("/classes", classesControllers.create);
-routes.post("/connections", connectionsController.create )
+routes.get("/connections", connectionsController.create );
 
 
 
