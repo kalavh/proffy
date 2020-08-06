c341f2e10f0ccec6fb69a0d3994e3e69fca49723
kalavh <45084197+kalavh@users.noreply.github.com>
2020-08-06 12:45:43 -0300

    now we are look for time befor filter

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -31,6 +31,7 @@ export default class ClassesController{
            .from('class_schedule')
            .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
            .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+           .whereRaw('`class_schedule`.`from` <= ??' ,[timeInMinutes])
        })
        .where('classes.subject' , '=' , subject)
        .join('users','classes.user_id','=','users.id')
