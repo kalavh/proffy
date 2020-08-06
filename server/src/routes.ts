558bfa6e09dc8f07cef0f91ea4e6e88dc50910e6
kalavh <jefferson_1996Th@hotmail.com>
2020-08-05 22:32:27 -0300

    make function to convert hour to minutes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,6 +3,12 @@ import db from './database/connection';
 
 const routes = express.Router()
 
+interface scheduleItem{
+    week_day: number;
+    from: string;
+    to: string;
+}
+
 routes.post("/classes", async (req,res) => {
 
     const {
@@ -24,12 +30,21 @@ routes.post("/classes", async (req,res) => {
 
     const user_id = insertedUsersIds[0]
 
-    await db('classes').insert({
+   const insertedClassesIds = await db('classes').insert({
         subject,
         cost,
         user_id,
         
     })
+
+    const class_id = insertedClassesIds[0]
+
+    const classSchedule = schedule.map((scheduleItem: scheduleItem) => {
+        return{
+
+        }
+    })
+
     return res.send("Ok"); 
 } )
 
