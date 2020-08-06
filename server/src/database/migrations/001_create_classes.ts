8b41525c180320b941e6126b19fb1818699a038c
kalavh <jefferson_1996Th@hotmail.com>
2020-08-05 21:33:27 -0300

    create with table users

--- server/src/database/migrations/001_create_classes.ts
+++ server/src/database/migrations/001_create_classes.ts
@@ -10,7 +10,7 @@ export async function up(knex: Knex) {
         table.integer('user_id')
         .notNullable()
         .references('id')
-        .inTable('yes')
+        .inTable('users')
     });
 }
 
