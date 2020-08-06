74293bccee861bd4ac200931d6f66d05ef565716
kalavh <jefferson_1996Th@hotmail.com>
2020-08-05 23:02:18 -0300

    make transation for don let useless things on other tables

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -23,38 +23,49 @@ routes.post("/classes", async (req,res) => {
     cost,
     schedule} = req.body
    
-    const trx = await db.transaction()
+    const trx = await db.transaction();
+   
+    try{ 
+        const insertedUsersIds = await trx('users').insert({
+            name,
+            avatar,
+            whatsapp,
+            bio,
+         }); 
     
-    const insertedUsersIds = await trx('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio,
-    }); 
+        const user_id = insertedUsersIds[0]
+    
+       const insertedClassesIds = await trx('classes').insert({
+            subject,
+            cost,
+            user_id,
+            
+        })
+    
+        const class_id = insertedClassesIds[0]
+    
+        const classSchedule = schedule.map((scheduleItem: scheduleItem) => {
+            return{
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: convertHourToMinutes(scheduleItem.from),
+                to:convertHourToMinutes(scheduleItem.to),
+            };
+        })
+    
+        await trx('class_schedule').insert(classSchedule)
+    
+        await trx.commit();
+        return res.status(201).send("Ok"); 
+    } catch (err) {
+        await trx.rollback();
+        return res.status(400).json({
+            error: 'Unexpected erro while create new class'
+        })
 
-    const user_id = insertedUsersIds[0]
-
-   const insertedClassesIds = await trx('classes').insert({
-        subject,
-        cost,
-        user_id,
-        
-    })
-
-    const class_id = insertedClassesIds[0]
-
-    const classSchedule = schedule.map((scheduleItem: scheduleItem) => {
-        return{
-            class_id,
-            week_day: scheduleItem.week_day,
-            from: convertHourToMinutes(scheduleItem.from),
-            to:convertHourToMinutes(scheduleItem.to),
-        };
-    })
-
-    await trx('class_schedule').insert(classSchedule)
-    return res.send("Ok"); 
-} )
+    }
+})
+   
 
 
 export default routes; 
