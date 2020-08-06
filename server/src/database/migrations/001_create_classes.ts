ce92ace35f7758dc733821b4c1deb54fc250a02d
kalavh <jefferson_1996Th@hotmail.com>
2020-08-05 21:31:34 -0300

    migrate create classes

--- server/src/database/migrations/001_create_classes.ts
+++ server/src/database/migrations/001_create_classes.ts
@@ -4,10 +4,8 @@ import Knex from 'knex';
 export async function up(knex: Knex) {
     return knex.schema.createTable('classes', table=> {
         table.increments('id').primary();
-        table.string('name').notNullable();
-        table.string('avatar').notNullable();
-        table.string('whatsapp').notNullable();
-        table.string('bio').notNullable();
+        table.string('subject').notNullable();
+        table.decimal('cost').notNullable();
     });
 }
 
