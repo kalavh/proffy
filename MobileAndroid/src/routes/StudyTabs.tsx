4a40c878a119bd095335b3840ca4a5f3a5306055
kalavh <jefferson_1996Th@hotmail.com>
2020-08-08 13:29:07 -0300

    style to bottom navigation

--- MobileAndroid/src/routes/StudyTabs.tsx
+++ MobileAndroid/src/routes/StudyTabs.tsx
@@ -11,7 +11,34 @@ const { Navigator, Screen } = createBottomTabNavigator();
 
 function StudyTabs() {
     return (
-        <Navigator>
+        <Navigator  tabBarOptions={{
+            style: {
+                elevation: 0,
+                shadowOpacity: 0 ,
+                height: 64,
+             },
+             tabStyle:{
+                 flexDirection: 'row',
+                 alignItems: 'center',
+                 justifyContent: 'center',
+             },
+             iconStyle: {
+                 flex: 0,
+                 width: 20,
+                 height: 20,
+             },
+             labelStyle:{
+                 fontSize: 13,
+                 marginLeft: 16,
+             },
+             inactiveBackgroundColor: '#fafafc',
+             activeBackgroundColor: '#ebebf5',
+             inactiveTintColor: '#c1bccc',
+             activeTintColor: '#32264d'
+
+             
+        }}>
+           
             <Screen name="Favorites"  component={Favorites} />
             <Screen name="TeacherList" component={TeacherList} />
         </Navigator>
