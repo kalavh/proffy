de16cd439b16b1b11cf2654b6622ee9e8f1b7998
kalavh <jefferson_1996Th@hotmail.com>
2020-08-07 23:16:16 -0300

    Create structer and styles for giveclasses page

--- MobileAndroid/src/pages/GiveClasses/index.tsx
+++ MobileAndroid/src/pages/GiveClasses/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-import { View }  from 'react-native'
+import { View,ImageBackground,Text }  from 'react-native'
+
+import styles from './styles'
+
+import giveClassesBgImage from '../../images/give-classes-background.png'
+import { RectButton } from 'react-native-gesture-handler';
 
 function GiveClasses() {
-    return( <View/> )
-}
+    return(
+         <View style={styles.container}>
+             <ImageBackground   
+             source={giveClassesBgImage} 
+             style={styles.content}>
+
+                <Text style={styles.title}>You Want to Be a teacher ??</Text>
+                <Text style={styles.description}>you need to register on our web plataform</Text>
+
+             </ImageBackground>
+
+             <RectButton style={styles.okButton}>
+                 <Text style={styles.okButtonText}> Okay </Text>
+
+             </RectButton>
+         </View>
+    
+    )}
 
 
 export default GiveClasses;
--- MobileAndroid/src/pages/GiveClasses/styles.ts
+++ MobileAndroid/src/pages/GiveClasses/styles.ts
@@ -1,9 +1,44 @@
-import React from 'react';
-import { View }  from 'react-native'
+import { StyleSheet } from 'react-native';
 
-function GiveClasses() {
-    return( <View/> )
-}
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#8257E5',
+        flex: 1,
+        justifyContent: 'center',
+        padding: 40,
+       
 
+    },
+    content: {
+        flex:1,
+        justifyContent:"center",
+        resizeMode: "contain",
+    },
+    title: {
+    fontSize: 32,
+    color: '#fff',
+    lineHeight:37,
+    maxWidth:180,
+    },
 
-export default GiveClasses;
+    description: {
+        marginTop: 24,
+        color: '#d4c2ff',
+        fontSize:16,
+        lineHeight: 26,
+        maxWidth:240
+    },
+    okButton:{
+        marginVertical: 40,
+        height:58,
+        backgroundColor: '#04d361',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    okButtonText: {
+        color: '#fff',
+        fontSize: 16,
+    }
+})
+
+export default styles
