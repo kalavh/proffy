8907bfea656e765e4bca2b6ee92f5f13770e6203
kalavh <jefferson_1996Th@hotmail.com>
2020-08-08 13:22:38 -0300

    make base for pages favorites and teacherList

--- MobileAndroid/src/pages/Favorites/index.tsx
+++ MobileAndroid/src/pages/Favorites/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import styles from './styles'
+
 function Favorites() {
-    return <View />
+    return (
+    <View style={styles.container}>
+       
+    </View>)
 }
 
 
--- MobileAndroid/src/pages/TeacherList/index.tsx
+++ MobileAndroid/src/pages/TeacherList/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
+import styles from './styles'
 
 function TeatcherList() {
     return (
-    <View>
-        <Text> teste </Text>
+    <View style={styles.container}>
+   
     </View>)
 }
 
