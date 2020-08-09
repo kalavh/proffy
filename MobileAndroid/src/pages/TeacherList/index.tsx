61587236f26f5eb32b1a70fc3729dfac089a7b11
kalavh <jefferson_1996Th@hotmail.com>
2020-08-09 14:11:14 -0300

    structre teacher list search

--- MobileAndroid/src/pages/TeacherList/index.tsx
+++ MobileAndroid/src/pages/TeacherList/index.tsx
@@ -3,13 +3,40 @@ import { View, ScrollView ,Text} from 'react-native';
 import styles from './styles'
 import PageHeader from '../../components/PageHeader';
 import TeactherItem from '../../components/TeacherItem';
+import { TextInput } from 'react-native-gesture-handler';
 
 function TeatcherList() {
     return (
     <View style={styles.container}>
 
         <PageHeader title="Teatchers">
-         <Text>teste</Text>
+         <View style={styles.searchForm}>
+           
+            <Text style={styles.label}>Subject</Text>
+            <TextInput 
+             style={styles.input}
+             placeholder='Choose Subject'
+            />
+
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Week Day</Text>
+                <TextInput 
+                style={styles.input}
+                placeholder='Choose WeekDay'
+                />
+              </View>
+
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Time</Text>
+                <TextInput 
+                style={styles.input}
+                placeholder='Choose Time'
+                />
+              </View>
+
+            </View>
+         </View>
         </PageHeader>
 
         <ScrollView 
